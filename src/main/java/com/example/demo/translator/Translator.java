16445cdf7f2f84ab199a091616947be6dfc4cb14
leonardas103 <wingdragon_9@hotmail.com>
2025-02-26 02:48:14 +0100

    remove unused query

--- src/main/java/com/example/demo/translator/Translator.java
+++ src/main/java/com/example/demo/translator/Translator.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 import static dev.cel.common.ast.CelExpr.ExprKind.Kind.*;
 
 public class Translator {
-    private static final Logger logger = LoggerFactory.getLogger(Translator2.class);
+    private static final Logger logger = LoggerFactory.getLogger(Translator.class);
 
     public static <T> Specification<T> translate(String celExpression, Class<T> entityClass) {
         return (Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
@@ -36,24 +36,23 @@ public class Translator {
                 CelCompiler celCompiler = CelCompilerFactory.standardCelCompilerBuilder()
                         .addVarDeclarations(new TypeGenerator(entityClass).build())
                         .build();
-                query.distinct(true);
-                return createPredicate(celCompiler.compile(celExpression).getAst().getExpr(), root, query, cb);
+                return createPredicate(celCompiler.compile(celExpression).getAst().getExpr(), root, cb);
             } catch (Exception e) {
                 throw new RuntimeException(e.getMessage(), e);
             }
         };
     }
 
-    private static <T> Predicate createPredicate(CelExpr expr, Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+    private static <T> Predicate createPredicate(CelExpr expr, Root<T> root, CriteriaBuilder cb) {
         logger.info("Processing expression of kind: {}", expr.getKind());
         switch (expr.getKind()) {
             case CALL:
-                return processFunctionCall(expr.call(), root, query, cb);
+                return processFunctionCall(expr.call(), root, cb);
             case IDENT:
                 return cb.conjunction();
             case SELECT:
                 return cb.equal(
-                        resolveFieldPath(expr.select().operand(), root, cb, query).get(expr.select().field()),
+                        resolveFieldPath(expr.select().operand(), root).get(expr.select().field()),
                         extractConstant(expr.select().operand())
                 );
             default:
@@ -61,7 +60,7 @@ public class Translator {
         }
     }
 
-    private static <T> Predicate processFunctionCall(CelExpr.CelCall callExpr, Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+    private static <T> Predicate processFunctionCall(CelExpr.CelCall callExpr, Root<T> root, CriteriaBuilder cb) {
         String operator = callExpr.function();
         List<CelExpr> arguments = callExpr.args();
         Optional<CelExpr> target = callExpr.target();
@@ -69,63 +68,63 @@ public class Translator {
         switch (operator) {
             case "_&&_":
                 return cb.and(
-                        createPredicate(arguments.get(0), root, query, cb),
-                        createPredicate(arguments.get(1), root, query, cb)
+                        createPredicate(arguments.get(0), root, cb),
+                        createPredicate(arguments.get(1), root, cb)
                 );
             case "_||_":
                 return cb.or(
-                    createPredicate(arguments.get(0), root, query, cb),
-                    createPredicate(arguments.get(1), root, query, cb)
-            );
+                        createPredicate(arguments.get(0), root, cb),
+                        createPredicate(arguments.get(1), root, cb)
+                );
             case "_==_":
                 return cb.equal(
-                        resolveFieldPath(arguments.get(0), root, cb, query),
+                        resolveFieldPath(arguments.get(0), root),
                         extractConstant(arguments.get(1))
                 );
             case "_!=_":
-                Path<?> resolvedFieldPath = resolveFieldPath(arguments.get(0), root, cb, query);
+                Path<?> resolvedFieldPath = resolveFieldPath(arguments.get(0), root);
                 Object value = extractConstant(arguments.get(1));
                 return cb.notEqual(resolvedFieldPath, value);
             case "_>_":
                 return cb.gt(
-                        resolveFieldPath(arguments.get(0), root, cb, query),
+                        resolveFieldPath(arguments.get(0), root),
                         (Number) extractConstant(arguments.get(1))
                 );
             case "_>=_":
                 return cb.ge(
-                        resolveFieldPath(arguments.get(0), root, cb, query),
+                        resolveFieldPath(arguments.get(0), root),
                         (Number) extractConstant(arguments.get(1))
                 );
             case "_<_":
                 return cb.lt(
-                        resolveFieldPath(arguments.get(0), root, cb, query),
+                        resolveFieldPath(arguments.get(0), root),
                         (Number) extractConstant(arguments.get(1))
                 );
             case "_<=_":
                 return cb.le(
-                        resolveFieldPath(arguments.get(0), root, cb, query),
+                        resolveFieldPath(arguments.get(0), root),
                         (Number) extractConstant(arguments.get(1))
                 );
             case "@in":
-                CriteriaBuilder.In<Object> inPredicate = cb.in(resolveFieldPath(arguments.get(0), root, cb, query));
+                CriteriaBuilder.In<Object> inPredicate = cb.in(resolveFieldPath(arguments.get(0), root));
                 arguments.get(1).list().elements().stream().map(Translator::extractConstant).forEach(inPredicate::value);
                 return inPredicate;
             case "startsWith":
-                return cb.like(resolveFieldPath(target.orElseThrow(), root, cb, query), arguments.get(0).constant().stringValue() +"%");
+                return cb.like(resolveFieldPath(target.orElseThrow(), root), arguments.get(0).constant().stringValue() +"%");
             case "contains":
-                return cb.like(resolveFieldPath(target.orElseThrow(), root, cb, query), "%" + arguments.get(0).constant().stringValue() +"%");
+                return cb.like(resolveFieldPath(target.orElseThrow(), root), "%" + arguments.get(0).constant().stringValue() +"%");
             default:
                 throw new UnsupportedOperationException("Unsupported CEL operator: " + operator);
         }
     }
 
-
-    private static <T, Y> Path<Y> resolveFieldPath(CelExpr expr, Root<T> root, CriteriaBuilder cb, CriteriaQuery<?> query) {
-        logger.info("Resolving path: {}", expr.toString().replace("\n", " "));
+    private static <T, Y> Path<Y> resolveFieldPath(CelExpr expr, Root<T> root) {
+        logger.info("Resolving path: {}", expr);
         switch (expr.getKind()) {
             case IDENT:
                 return root.get(expr.ident().name());
             case SELECT:
+                return resolveFieldPath(expr.select().operand(), root).get(expr.select().field());
             default:
                 throw new IllegalArgumentException("Unsupported field path expression: " + expr.getKind());
         }
